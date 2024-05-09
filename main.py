8b358c7a877dea653839dd5b049f8cdc2e648652
Deepa_Git <deepaghivari14@gmail.com>
2024-05-09 15:16:13 +0530

    Create instances

--- main.py
+++ main.py
@@ -3,13 +3,36 @@ import pandas as pd
 df=pd.read_csv("hotel.csv")
 
 class Hotel:
-    pass
+    def __init__(self,id):
+        pass
+
+    def book(self):
+        pass
+
+    def available(self):
+        pass
 
 class ReservationTicket:
-    pass
+    def __init__(self,customer_name, hotel_object):
+        pass
+
+    def generate(self):
+        pass
 
 
 
+
+
+id=input("Enter id of the hotel:")
+hotel=Hotel(id)
+
+if hotel.available():
+    hotel.book()
+    name=input("Enter your name: ")
+    reservation_tcket = ReservationTicket(name, hotel)
+    print(reservation_tcket.generate())
+else:
+    print("Hotel is not free")
 
 
 
