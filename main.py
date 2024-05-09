79fba72f1127abef4e0e8474c47464cede40dda2
Deepa_Git <deepaghivari14@gmail.com>
2024-05-09 15:29:13 +0530

    Implement the methods of the Reservation Ticket Class

--- main.py
+++ main.py
@@ -22,22 +22,29 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self,customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Here are your reservation data:
+        Name : {self.customer_name}
+        Hotel Name : {self.hotel.name}
+        """
+        return content
 
 
 
 
-
-id=input("Enter id of the hotel:")
-hotel=Hotel(id)
+print(df)
+hotel_ID=input("Enter id of the hotel:")
+hotel=Hotel(hotel_ID)
 
 if hotel.available():
     hotel.book()
     name=input("Enter your name: ")
-    reservation_tcket = ReservationTicket(name, hotel)
+    reservation_tcket = ReservationTicket(customer_name=name, hotel_object=hotel)
     print(reservation_tcket.generate())
 else:
     print("Hotel is not free")
