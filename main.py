28277855d21fb0c78a407c2a930ce9250b702e77
Deepa_Git <deepaghivari14@gmail.com>
2024-05-09 15:29:13 +0530

    Implement the methods of the Reservation Ticket Class

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ class Hotel:
         self.hotel_id = hotel_id
 
     def book(self):
-        """Boo a hotel by changing iits availability to no"""
+        """Book a hotel by changing its availability to no"""
         df.loc[df['id']==self.hotel_id, "available"] = "no"
         df.to_csv("hotel.csv", index=False)
 
     def available(self):
-        """Chcek if the hotel is available"""
+        """Check if the hotel is available"""
         availability = df.loc[df["id"]==self.hotel_id,"available"].sqeeze()
         if availability == "yes":
             return True
@@ -22,22 +22,29 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self,customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Here are your reservation data:
+        Name : {self.customer_name}
+        Hotel Name : {self.hotel.name}
+        """
+        return content
 
 
 
 
-
-id=input("Enter id of the hotel:")
-hotel=Hotel(id)
+print(df)
+hotel_ID=input("Enter id of the hotel:")
+hotel=Hotel(hotel_ID)
 
 if hotel.available():
     hotel.book()
     name=input("Enter your name: ")
-    reservation_tcket = ReservationTicket(name, hotel)
+    reservation_tcket = ReservationTicket(customer_name=name, hotel_object=hotel)
     print(reservation_tcket.generate())
 else:
     print("Hotel is not free")
